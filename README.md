5156063e222394e7eb80018c5f63aff16f7a272d
CaioslppUO <CaioslppUO@gmail.com>
2020-11-02 17:22:25 -0300

    Refatoração setup.py.
    - Quebra de funções grandes.
    - Mudanças em comentários.

--- README.md
+++ README.md
@@ -31,4 +31,4 @@
 
     * Receive Command -> Command Priority Decider -> Command Standardize -> Command Assembler -> Send Command.
 
-# Versão: 4.8
+# Versão: 4.9
--- src/agrobot/src/robot_nodes/setup.py
+++ src/agrobot/src/robot_nodes/setup.py
@@ -59,56 +59,64 @@ def get_selected_robot_model() -> str:
         services.do_log_error("Could not read setup.json. "+str(e),current_file)
     return robot_model
 
+## Carrega os parâmetros para a configuração do mini_robot.
+def load_mini_robot() -> None:
+    global gpio_left_wheel_1,gpio_left_wheel_2,gpio_right_wheel_1,gpio_right_wheel_2
+    try:
+        with open(project_dir+"src/" + robot_config_dir + "/robot_models/"+str(rosparam.get_param("robot_model"))+".json","r") as file:
+            json_object = json.load(file)
+            file.close()
+            gpio_left_wheel_1 = json_object['gpio_left_wheel_1']
+            gpio_left_wheel_2 = json_object['gpio_left_wheel_2']
+            gpio_right_wheel_1 = json_object['gpio_right_wheel_1']
+            gpio_right_wheel_2 = json_object['gpio_right_wheel_2']
+            if(gpio_left_wheel_1 != -1 and gpio_left_wheel_2 != -1 and gpio_right_wheel_1 != -1 and gpio_right_wheel_2 != -1):
+                rosparam.set_param("gpio_left_wheel_1",str(gpio_left_wheel_1))
+                rosparam.set_param("gpio_left_wheel_2",str(gpio_left_wheel_2))
+                rosparam.set_param("gpio_right_wheel_1",str(gpio_right_wheel_1))
+                rosparam.set_param("gpio_right_wheel_2",str(gpio_right_wheel_2))
+                services.do_log_info("Robot model loaded successfully from " + str(rosparam.get_param("robot_model")) + ".json",current_file)
+            else:
+                rosparam.set_param("gpio_left_wheel_1","-1")
+                rosparam.set_param("gpio_left_wheel_2","-1")
+                rosparam.set_param("gpio_right_wheel_1","-1")
+                rosparam.set_param("gpio_right_wheel_2","-1")
+                services.do_log_error("Could not load robot model (" + str(rosparam.get_param("robot_model")) + ") properly. Check for invalid values.",current_file)
+    except Exception as e:
+        services.do_log_error("Could not load robot_model (" + str(rosparam.get_param("robot_model")) + "). " + str(e),current_file) 
+
+## Carrega os parâmetros para a configuração do agrobot ou corona_killer.
+def load_robot() -> None:
+    global number_of_hoverboard_boards,extra_module_gpio_pinout
+    try:
+        with open(project_dir+"src/" + robot_config_dir + "/robot_models/"+str(rosparam.get_param("robot_model"))+".json","r") as file:
+            json_object = json.load(file)
+            file.close()
+            number_of_hoverboard_boards = json_object['number_of_hoverboard_boards']
+            extra_module_gpio_pinout = json_object['extra_module_gpio_pinout']
+            if(number_of_hoverboard_boards != -1 and extra_module_gpio_pinout != -1):
+                rosparam.set_param("hoverboard_boards",str(number_of_hoverboard_boards))
+                rosparam.set_param("module_pinout",str(extra_module_gpio_pinout))
+                services.do_log_info("Robot model loaded successfully from " + str(rosparam.get_param("robot_model")) + ".json",current_file)
+            else:
+                rosparam.set_param("hoverboard_boards","0")
+                rosparam.set_param("module_pinout","-1")
+                services.do_log_error("Could not load robot model (" + str(rosparam.get_param("robot_model")) + ") properly. Check for invalid values.",current_file)
+    except Exception as e:
+        services.do_log_error("Could not load robot_model (" + str(rosparam.get_param("robot_model")) + "). " + str(e),current_file)
+
 ## Lê e carrega o modelo de robô selecionado.
-def get_robot_model():
+def load_robot_model():
     robot_model = get_selected_robot_model()
     if(robot_model == "corona_killer" or robot_model == "agrobot"):
-        global number_of_hoverboard_boards,extra_module_gpio_pinout
-        try:
-            with open(project_dir+"src/" + robot_config_dir + "/robot_models/"+str(rosparam.get_param("robot_model"))+".json","r") as file:
-                json_object = json.load(file)
-                file.close()
-                number_of_hoverboard_boards = json_object['number_of_hoverboard_boards']
-                extra_module_gpio_pinout = json_object['extra_module_gpio_pinout']
-                if(number_of_hoverboard_boards != -1 and extra_module_gpio_pinout != -1):
-                    rosparam.set_param("hoverboard_boards",str(number_of_hoverboard_boards))
-                    rosparam.set_param("module_pinout",str(extra_module_gpio_pinout))
-                    services.do_log_info("Robot model loaded successfully from " + str(rosparam.get_param("robot_model")) + ".json",current_file)
-                else:
-                    rosparam.set_param("hoverboard_boards","0")
-                    rosparam.set_param("module_pinout","-1")
-                    services.do_log_error("Could not load robot model (" + str(rosparam.get_param("robot_model")) + ") properly. Check for invalid values.",current_file)
-        except Exception as e:
-            services.do_log_error("Could not load robot_model (" + str(rosparam.get_param("robot_model")) + "). " + str(e),current_file)
+        load_robot()
     elif(robot_model == "mini_robot"):
-        global gpio_left_wheel_1,gpio_left_wheel_2,gpio_right_wheel_1,gpio_right_wheel_2
-        try:
-            with open(project_dir+"src/" + robot_config_dir + "/robot_models/"+str(rosparam.get_param("robot_model"))+".json","r") as file:
-                json_object = json.load(file)
-                file.close()
-                gpio_left_wheel_1 = json_object['gpio_left_wheel_1']
-                gpio_left_wheel_2 = json_object['gpio_left_wheel_2']
-                gpio_right_wheel_1 = json_object['gpio_right_wheel_1']
-                gpio_right_wheel_2 = json_object['gpio_right_wheel_2']
-                if(gpio_left_wheel_1 != -1 and gpio_left_wheel_2 != -1 and gpio_right_wheel_1 != -1 and gpio_right_wheel_2 != -1):
-                    rosparam.set_param("gpio_left_wheel_1",str(gpio_left_wheel_1))
-                    rosparam.set_param("gpio_left_wheel_2",str(gpio_left_wheel_2))
-                    rosparam.set_param("gpio_right_wheel_1",str(gpio_right_wheel_1))
-                    rosparam.set_param("gpio_right_wheel_2",str(gpio_right_wheel_2))
-                    services.do_log_info("Robot model loaded successfully from " + str(rosparam.get_param("robot_model")) + ".json",current_file)
-                else:
-                    rosparam.set_param("gpio_left_wheel_1","-1")
-                    rosparam.set_param("gpio_left_wheel_2","-1")
-                    rosparam.set_param("gpio_right_wheel_1","-1")
-                    rosparam.set_param("gpio_right_wheel_2","-1")
-                    services.do_log_error("Could not load robot model (" + str(rosparam.get_param("robot_model")) + ") properly. Check for invalid values.",current_file)
-        except Exception as e:
-            services.do_log_error("Could not load robot_model (" + str(rosparam.get_param("robot_model")) + "). " + str(e),current_file)        
+        load_mini_robot()
 
 ## Executa as rotinas de setup.
 if __name__ == "__main__":
     if(services.wait_for_services_availability()):
         get_version()
-        get_robot_model()
+        load_robot_model()
     else:
         services.do_log_error("Time limit reached when waiting for used services to respond.","setup.py")
