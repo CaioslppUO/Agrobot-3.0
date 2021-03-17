b8749ce45d4daee42297f67c476a86abd748d1fa
CaioslppUO <caioslppuo@gmail.com>
2021-03-17 17:54:03 -0300

    Correção script controle direção robô.
    - Atualizado o script que controla o movimento dos motores que controlam a direção do robô.
    - Não foi testado, necessário testar com os motores.

--- src/agrobot/src/robot_nodes/control_direction.py
+++ src/agrobot/src/robot_nodes/control_direction.py
@@ -9,6 +9,7 @@ import rospy
 import RPi.GPIO as GPIO
 from agrobot.msg import complete_command
 from robot_utils import testing
+from std_msgs.msg import String
 import time
 
 # Injeção de dependência.
@@ -23,8 +24,11 @@ GPIO.setmode(GPIO.BOARD)
 rospy.init_node('control_direction', anonymous=True)
 
 # Variáveis de configuração.
-motor_1: int = 16
-motor_2: int = 19
+motor_1: int = 16 # Raspberry pin 16
+motor_2: int = 19 # Raspberrry pin 19
+encoder: int = 0
+straight_angle: tuple = (-5,-4,-3,-2,-1,1,2,3,4,5) # Posição em que o robô não está virando.
+max_turn_angle: dict = {'left':-300,'right':300}
 
 GPIO.setup(motor_1, GPIO.OUT)
 GPIO.setup(motor_2, GPIO.OUT)
@@ -32,68 +36,54 @@ GPIO.setup(motor_2, GPIO.OUT)
 def turn_right() -> None:
     GPIO.output(motor_1, GPIO.LOW)
     GPIO.output(motor_2, GPIO.HIGH)
-    time.sleep(0.1)
+    time.sleep(0.05)
     GPIO.output(motor_1, GPIO.LOW)
     GPIO.output(motor_2, GPIO.LOW)
 
 def turn_left() -> None:
     GPIO.output(motor_1, GPIO.HIGH)
     GPIO.output(motor_2, GPIO.LOW)
-    time.sleep(0.1)
+    time.sleep(0.05)
     GPIO.output(motor_1, GPIO.LOW)
     GPIO.output(motor_2, GPIO.LOW)
 
-def turn_all_right() -> None:
-    GPIO.output(motor_1, GPIO.LOW)
-    GPIO.output(motor_2, GPIO.HIGH)
-    time.sleep(0.2)
-    GPIO.output(motor_1, GPIO.LOW)
-    GPIO.output(motor_2, GPIO.LOW)
-    pass
-
-def turn_all_left() -> None:
-    GPIO.output(motor_1, GPIO.HIGH)
-    GPIO.output(motor_2, GPIO.LOW)
-    time.sleep(0.2)
+def straight() -> None:
     GPIO.output(motor_1, GPIO.LOW)
     GPIO.output(motor_2, GPIO.LOW)
 
-def turn_ahead() -> None:
-    pass
-
-def control_direction(factor: int) -> None:
+def control_direction(steer: int, encoder: int) -> None:
     """Chama a função que vira o robô de acordo com o fator."""
-    if(factor == -1):
-        turn_left()
-    elif(factor == 1):
-        turn_right()
-    elif(factor == -2):
-        turn_all_left()
-    elif(factor == 2):
-        turn_all_right()
-    else:
-        turn_ahead()
-
-def get_steer_factor(steer: int) -> int:
-    """Retorna o fator de viragem do robô. -1 caso esteja entre -50 e 0. 1 caso
-        entre 0 e 50. -2 caso entre -100 e -50. 2 caso entre 50 e 100."""
-    if(steer > -50 and steer < -10):
-        return -1
-    elif(steer > 10 and steer < 50):
-        return 1
-    elif(steer > -100 and steer < -50):
-        return -2
-    elif(steer > 50 and steer < 100):
-        return 2
-    return 0
+    # Robô indo reto.
+    if(steer in straight_angle):
+        if(encoder < int(straight_angle[0])): # Corrigindo a direção.
+            turn_right()
+        elif(encoder > int(straight_angle[-1])): # Corrigindo a direção.
+            turn_left()
+        else:
+            straight()
+    elif(steer < int(straight_angle[0])): # Virando para a esquerda.
+        if(encoder > max_turn_angle['left']):
+            turn_left()
+    else: # Virando para a direita.
+        if(encoder < max_turn_angle['right']):
+            turn_right()
 
 def control_robot_callback(data: complete_command) -> str:
     """Envia o comando de controle recebido para os motores que controlam a 
         direção do robô."""
     steer: int = - int(data.move.angular.z)
-    factor: int = get_steer_factor(steer)
+    factor: int = 10
     control_direction(factor)
     pass
+
+def encoder_callback(data: String) -> None:
+    """Armazena o valor lido do tópico encoder."""
+    global encoder 
+    encoder = int(data.data)
+
+def listen_encoder() -> None:
+    """Esuta o tóico encoder e processa o valor lido."""
+    rospy.Subscriver('encoder',String,encoder_callback)
 
 def listen_control_robot() -> None:
     """Escuta o tópico control_robot e processa os comandos recebidos."""
@@ -101,6 +91,7 @@ def listen_control_robot() -> None:
 
 if __name__ == "__main__":
     try:
+        listen_encoder()
         listen_control_robot()
         rospy.spin()
     except Exception as e:
